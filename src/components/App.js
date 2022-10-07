6a7d29753eb45ed6f18cbe30d0a88dd81f138b9b
Omkar patil <omkarp666@gmail.com>
2022-10-07 19:01:21 +0530

    Marco Polo

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
 import '../styles/App.css';
+
+
 const App = () => {
+  const [alternate , setAlternate] = useState(false);
   
+  const handleClick = () =>{
+    if(alternate == false){
+      setAlternate(true)
+    }else{
+      setAlternate(false)
+    }
+  }
   return (
     <div id="main">
-
+        <h1 id="marco-polo" >{alternate ? "Polo" : "Marco"}</h1>
+        <button id='marco-polo-toggler'  onClick={handleClick}>{alternate ? "Marco" : "Polo"}</button>
     </div>
   )
 }
